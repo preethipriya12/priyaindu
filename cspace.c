e1ed5e0006d20caa4773d069ccfc0126f4510c55
preethipriya12 <43203535+preethipriya12@users.noreply.github.com>
2018-12-26 15:33:52 +0530

    Create cspace.c

---
+++ cspace.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char b[50];
+	int i,count=0;
+	gets(a);
+	
+	for(i=0;b[i]!='\0';i++)
+	{
+		if(b[i] == ' ')
+		count++;
+		
+		
+	}
+	printf("%d",count);
+	return 0;
+}
